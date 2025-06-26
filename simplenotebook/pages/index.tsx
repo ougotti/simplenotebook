fe737836efaae1e60100e2cc25d238e5182558d3
ougotti <pxr00270@nifty.com>
2025-06-26 16:17:34 +0900

    Expand Next.js template

--- simplenotebook/pages/index.tsx
+++ simplenotebook/pages/index.tsx
@@ -1,14 +1,14 @@
 import Head from 'next/head'
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <>
+    <div className={styles.container}>
       <Head>
         <title>Simplenotebook</title>
+        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <h1>Hello, Next.js!</h1>
-      </main>
-    </>
+      <h1 className={styles.title}>Hello, Next.js!</h1>
+    </div>
   )
 }
