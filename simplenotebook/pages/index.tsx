074dcdfe02ffcc46b9b4cf3557ec9772fad1941b
ougotti <pxr00270@nifty.com>
2025-06-26 16:07:34 +0900

    Initialize Next.js project

---
+++ simplenotebook/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Simplenotebook</title>
+      </Head>
+      <main>
+        <h1>Hello, Next.js!</h1>
+      </main>
+    </>
+  )
+}
